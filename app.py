820a87fbc098ed75c76fe23c096ad7081740b42f
KulkarniSuraj <kulkarnisurajv@gmail.com>
2019-05-01 07:25:23 +0530

    adds file upload feature DocSum.html and summarization code refactored, summary of user text can be displayed on same page and minor bug fixes

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, url_for
+import text_summarizer
+import os
 
 app = Flask(__name__)
 
@@ -15,11 +17,25 @@ def services():
 def web_summary():
 	return render_template("WebSum.html")
 
-
-
 @app.route("/docSummary")
 def doc_summary():
 	return render_template("DocSum.html")
+
+@app.route("/webSummary", methods=['POST', 'GET'])
+def my_url():
+	url = request.form['urltext']
+	return text_summarizer.get_summary(url.strip())
+	
+
+@app.route('/docSummary', methods=['POST'])
+def submit_text():
+	user_text = request.form['userText']
+	return render_template("DocSum.html", p=text_summarizer.summarize_text(user_text))
+
+@app.route('/docSummary', methods=['POST', 'GET'])
+def upload_file():
+	pass
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
--- text_summarizer.py
+++ text_summarizer.py
@@ -6,9 +6,10 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-def filter_text(article):    
+
+def filter_text(paragraph_list):
     article_text = ""
-    for para in article:
+    for para in paragraph_list:
         article_text += para.text
     # removing brackets and replacing them with spaces
     article_text = re.sub(r'\[[0-9a-zA-Z]*\]', ' ', article_text)
@@ -16,13 +17,15 @@ def filter_text(article):
     article_text = re.sub(r'\s+', ' ', article_text)
     return article_text
 
+
 def sentence_tokenization(text_data):
     custom_sent_tokenizer = PunktSentenceTokenizer(train_text=text_data)
     tokenized_sent = []
     # tokenize sentences
-    for line in custom_sent_tokenizer.tokenize(article_text):
+    for line in custom_sent_tokenizer.tokenize(text_data):
         tokenized_sent.append(line)
     return tokenized_sent
+
 
 def word_tokenization(text):
     tokenized_words = []
@@ -31,10 +34,11 @@ def word_tokenization(text):
         tokenized_words.append(word)
     return tokenized_words
 
+
 def calculate_word_frequencies(words):
     # finding the frequencies of words
-    word_frequencies = {}  
-    for word in words:  
+    word_frequencies = {}
+    for word in words:
         if word not in stop_words:
             if word not in word_frequencies.keys():
                 word_frequencies[word] = 1
@@ -42,7 +46,8 @@ def calculate_word_frequencies(words):
                 word_frequencies[word] += 1
     return word_frequencies
 
-def calculate_sent_scores(tokenized_sent):    
+
+def calculate_sent_scores(tokenized_sent, word_frequencies):
     sent_scores = {}
 
     # calculating sentence scores
@@ -51,20 +56,22 @@ def calculate_sent_scores(tokenized_sent):
             if word in word_frequencies.keys():
                 if len(sent.split(' ')) < 30:
                     if sent not in sent_scores.keys():
-                        sent_scores[sent]  = word_frequencies[word]
+                        sent_scores[sent] = word_frequencies[word]
                     else:
                         sent_scores[sent] += word_frequencies[word]
 
     return sent_scores
 
+
 def normalize_word_freq(word_frequencies):
     # finding the maximum frequency
     maximum_frequncy = max(word_frequencies.values())
     # dividing each freq with max frequency
-    for word in word_frequencies.keys():  
+    for word in word_frequencies.keys():
         word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
 
     return word_frequencies
+
 
 def summarize(sent_scores):
     # finding top sentences based on their frequencies
@@ -73,22 +80,34 @@ def summarize(sent_scores):
     summary = ' '.join(summary_sentences)
     return summary
 
+
+def wiki_info(url):
+    source = requests.get(url).text  # source code of given web page
+    soup = BeautifulSoup(source, 'lxml')
+    article = soup.find_all('p')  # list of all paragraphs on page
+    return article
+
+
 # driver code
-stop_words = set(stopwords.words("english")) # stop words collection
-source = requests.get("https://en.wikipedia.org/wiki/Ketosis").text # source code of given web page
-soup = BeautifulSoup(source, 'lxml')
-article = soup.find_all('p') # list of all paragraphs on page
-
-article_text = filter_text(article)
-tokenized_sents = sentence_tokenization(article_text)
-tokenized_words = word_tokenization(article_text)
-word_frequencies = calculate_word_frequencies(tokenized_words)
-word_frequencies = normalize_word_freq(word_frequencies)
-sent_scores = calculate_sent_scores(tokenized_sents)
-summarized_text = summarize(sent_scores)
-
-print(summarized_text) 
-print("\n", '~' * 20, 'original article', '~' * 20, "\n")
-print(article_text)
+stop_words = set(stopwords.words("english"))  # stop words collection
 
 
+def get_summary(url="https://en.wikipedia.org/wiki/Ketosis"):
+    article = wiki_info(url)
+    article_text = filter_text(article)
+    return summarize_text(article_text)
+
+
+def summarize_text(article_text):
+    tokenized_sents = sentence_tokenization(article_text)
+    tokenized_words = word_tokenization(article_text)
+    word_frequencies = calculate_word_frequencies(tokenized_words)
+    sent_scores = calculate_sent_scores(tokenized_sents, word_frequencies)
+    word_frequencies = normalize_word_freq(word_frequencies)
+    summarized_text = summarize(sent_scores)
+    return summarized_text
+
+
+# print(summarized_text)
+# print("\n", '~' * 20, 'original article', '~' * 20, "\n")
+# print(article_text)
